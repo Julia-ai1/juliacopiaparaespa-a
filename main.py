7bff11c718d90a98089cc74fd00f07d8eb46a3f8
Carlos <cadamar1236@gmail.com>
2024-08-30 00:31:57 +0200

    main

--- main.py
+++ main.py
@@ -425,7 +425,7 @@ def charge():
         return str(e)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
 
 
 
