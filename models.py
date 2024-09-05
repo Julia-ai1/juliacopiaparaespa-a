1bb1dbb4d7d950177ebf00e4d585e2b108d15c99
Carlos <cadamar1236@gmail.com>
2024-09-05 12:23:10 +0200

    subidaaversivadatabase3

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
