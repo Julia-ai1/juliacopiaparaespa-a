0aa56103071670f0445907d01ab208c8594943d8
Carlos <cadamar1236@gmail.com>
2024-10-20 10:24:16 +0200

    subida con otro enlace

--- videos.py
+++ videos.py
@@ -2,7 +2,7 @@
 videos = [
     {
         "title": "Clase 1: Álgebra Básica",
-        "url": "https://www.youtube.com/watch?v=fxbCHn6gE3U",
+        "url": "https://www.youtube.com/watch?v=okumzdhTId0",
         "topic": "Álgebra",
         "course": "Curso 1",
         "segment": "Segmento A",
